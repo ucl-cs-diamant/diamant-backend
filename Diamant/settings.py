1217a16d5736fe6981bca8d4a79e86ea63ae934a
daohe <daohengliu@gmail.com>
2021-07-13 12:55:01 +0100

    Update initial configuration, add DRF as dependency, create new app (game_engine)
    Took 13 minutes

--- Diamant/settings.py
+++ Diamant/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,10 @@ WSGI_APPLICATION = 'Diamant.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': str(Path.joinpath(BASE_DIR, 'mariadb.cnf')),
+        },
     }
 }
 
---
+++ game_engine/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GameEngineConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'game_engine'
