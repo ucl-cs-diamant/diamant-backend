31b44bfe11ebd225bf2551e9e140cc1340052acc
Joel-Morgan <jojomorgan206@gmail.com>
2021-07-31 19:04:51 +0100

    implement matchmaking changes and tests for tasks.py

---
+++ game_engine/tests/test_models.py
@@ -0,0 +1,51 @@
+from django.test import TestCase
+import game_engine.models as models
+
+
+# Create your tests here.
+
+
+class UserTestCase(TestCase):
+    def setUp(self):
+        self.user = models.User.objects.create(student_id=28588828,
+                                               email_address="hello@ucl.ac.uk",
+                                               github_username="hello")
+
+    def test_user_types(self):
+        self.assertEqual(type(self.user.student_id), int)
+        self.assertEqual(type(self.user.email_address), str)
+        self.assertEqual(type(self.user.github_username), str)
+
+
+class UserPerformanceTestCase(TestCase):
+    def setUp(self):
+        self.user = models.User.objects.create(student_id=28588828,
+                                               email_address="hello@ucl.ac.uk",
+                                               github_username="hello")
+        self.up_instance = models.UserPerformance.objects.create(user=self.user)
+
+    def test_up_defaults(self):
+        self.assertEqual(self.up_instance.mmr, 25.00)
+        self.assertEqual(type(self.up_instance.mmr), float)
+
+        self.assertEqual(self.up_instance.confidence, 8.33333)
+        self.assertEqual(type(self.up_instance.confidence), float)
+
+        self.assertEqual(self.up_instance.games_played, 0)
+        self.assertEqual(type(self.up_instance.games_played), int)
+
+
+class MatchTestCase(TestCase):
+    def setUp(self):
+        self.user = models.User.objects.create(student_id=28588828,
+                                               email_address="hello@ucl.ac.uk",
+                                               github_username="hello")
+        self.match = models.Match.objects.create(players=[self.user.id])
+
+    def test_match_defaults(self):
+        self.assertEqual(self.match.allocated, None)
+        self.assertFalse(self.match.in_progress)
+        self.assertFalse(self.match.over)
+        self.assertEqual(self.match.players[0], self.user.id)
+
+# todo: make more in depth tests (negative), and test MatchResult once MatchPlayerField is figured out
---
+++ game_engine/tests/test_tasks.py
@@ -0,0 +1,91 @@
+from datetime import timedelta
+
+from django.utils import timezone
+from django.test import TestCase
+from django.core.files import File
+import game_engine.models as models
+import mock
+import game_engine.tasks as tasks
+
+
+class TestTasks(TestCase):
+    def setUp(self):
+        self.user_list = []
+        self.user_code_list = []
+        self.user_performance_list = []
+        self.mock_file = mock.MagicMock(spec=File)
+        self.mock_file.name = "testing.py"
+        for i in range(4):
+            user = models.User.objects.create(student_id=i,
+                                              email_address="{name}@ucl.ac.uk".format(name=i),
+                                              github_username="{name}".format(name=i))
+            self.user_list.append(user)
+            user.save()
+
+            user_code = models.UserCode.objects.create(user=user,
+                                                       source_code=self.mock_file.name,
+                                                       commit_time=timezone.now())
+            self.user_code_list.append(user_code)
+            user_code.save()
+
+            user_performance = models.UserPerformance.objects.create(user=user,
+                                                                     mmr=25.00 + i,
+                                                                     confidence=8.33333 - i)
+            self.user_performance_list.append(user_performance)
+            user_performance.save()
+
+        # dummy timeout value since messing around with environment variables is a pain potentially
+        self.timeout = float(60)
+
+    def test_match_making_with_players(self):
+        tasks.matchmake()
+        match = models.Match.objects.all().first()
+        self.assertEqual(len(match.players), 4)
+
+        players_ingame = models.UserCode.objects.filter(is_in_game=True)
+        self.assertEqual(len(players_ingame), 4)
+
+    def test_match_making_no_players(self):
+        for user in self.user_code_list:
+            user.delete()
+        tasks.matchmake()
+        self.assertFalse(models.Match.objects.all().exists())
+
+    def test_match_scrubbing(self):
+        tasks.matchmake()
+        models.Match.objects.all().update(in_progress=True)
+        models.Match.objects.filter(in_progress=True).update(
+            allocated=(timezone.now() - timedelta(seconds=self.timeout + 5)))
+        tasks.scrub_dead_matches()
+
+        self.assertFalse(models.Match.objects.all().exists())
+        self.assertFalse(models.UserCode.objects.filter(is_in_game=True).exists())
+
+    def test_extract_players(self):
+        player_list = models.UserPerformance.objects.all().order_by('mmr')
+
+        sublist = tasks.extract_players(player_list, 1, 4)
+        self.assertEqual(sublist, list(player_list.values_list('user', flat=True)))
+
+        sublist = tasks.extract_players(player_list, 1, 7)
+        self.assertIsNone(sublist)
+
+        sublist = tasks.extract_players(player_list, 3, 3)
+        self.assertEqual(sublist, list(player_list.values_list('user', flat=True)[1:]))
+
+    def test_evaluate_quality(self):
+        user_performance = models.UserPerformance.objects.all().order_by('mmr')
+
+        sublist = tasks.extract_players(user_performance, 1, 3)
+        quality = tasks.evaluate_quality(user_performance, sublist)
+        self.assertEqual(quality, 0.24008241712969108)
+
+    def test_find_players(self):
+        user_code = models.UserCode.objects.all()
+        player_list = tasks.find_players(user_code, 4)
+        self.assertEqual(player_list, list(models.UserCode.objects.values_list('user', flat=True)))
+
+        player_list = tasks.find_players(user_code, 3)
+        self.assertEqual(len(player_list), 3)
+
+
