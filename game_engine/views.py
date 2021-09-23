dd43935fc0f994ae93708d737238dfbe0b6c7545
daohe <daohengliu@gmail.com>
2021-09-23 01:53:53 +0100

    game_engine gamerunner api: refactor match reporting, add download endpoint for user code
    Took 4 hours 59 minutes

--- game_engine/views.py
+++ game_engine/views.py
@@ -20,6 +20,7 @@ from trueskill import Rating, rate
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
     # permission_classes = [permissions.IsAuthenticated]
 
     @action(detail=True)
@@ -30,15 +31,15 @@ class UserViewSet(viewsets.ModelViewSet):
         serializer = UserCodeSerializer(objects, many=True, context={'request': request})
         return Response(serializer.data)
 
-    @action(detail=True, permission_classes=[])
-    def latest_code(self, request, pk=None):
-        user_code = UserCode.objects.filter(user=pk)
-        if user_code.exists():
-            latest_code = user_code.latest("commit_time")
-            resp = HttpResponse(latest_code.source_code.file, content_type="application/octet-stream")
-            resp['Content-Disposition'] = f'attachment; filename={os.path.basename(latest_code.source_code.name)}'
-            return resp
-        return Response(status=status.HTTP_404_NOT_FOUND)  # this should not happen to the game runner (matchmaking)
+    # @action(detail=True, permission_classes=[])
+    # def latest_code(self, request, pk=None):
+    #     user_code = UserCode.objects.filter(user=pk)
+    #     if user_code.exists():
+    #         latest_code = user_code.latest("commit_time")
+    #         resp = HttpResponse(latest_code.source_code.file, content_type="application/octet-stream")
+    #         resp['Content-Disposition'] = f'attachment; filename={os.path.basename(latest_code.source_code.name)}'
+    #         return resp
+    #     return Response(status=status.HTTP_404_NOT_FOUND)  # this should not happen to the game runner (matchmaking)
 
     @action(detail=True)
     def performance_list(self, request, pk=None):
@@ -77,62 +78,76 @@ class MatchViewSet(viewsets.ModelViewSet):
         except Match.DoesNotExist:
             return Response({"ok": False, "message": "Match has been timed out"}, status=status.HTTP_410_GONE)
 
-        if "winners" in request.data and isinstance(request.data["winners"], list):
+        if "outcome" not in request.data:
+            return Response({"ok": False, "message": "No outcome specified"}, status=status.HTTP_400_BAD_REQUEST)
+        if request.data["outcome"] == "ok":
+            if not isinstance(request.data.get("winners", None), list):
+                return Response({"ok": False, "message": "No winners provided"},
+                                status=status.HTTP_400_BAD_REQUEST)
+
             match_players = match.players
             winners = request.data["winners"]
-            # losers = set(match_players).difference(set(winners))
-            if set(winners).issubset(set(match_players)):
-                if "match_history" in request.data and isinstance(request.data["match_history"], list):
-                    match_result = MatchResult()
-                    match_result.time_started = match.allocated
-                    match_result.players = Match.objects.get(pk=pk).players
-                    match_result.winners = request.data["winners"]
-                    match_result.match_events = request.data["match_history"]
-                    match_result.save()
 
-                    match.delete()
+            if not set(winners).issubset(set(match_players)):
+                return Response({"ok": False, "message": "One or more winner not part of match"},
+                                status=status.HTTP_400_BAD_REQUEST)
 
-                    rating_group = []  # list of player ratings and their win/loss pos
-                    ranks = []
-
-                    for player in match_players:
-                        up_instance, created = UserPerformance.objects.get_or_create(user=User.objects.get(pk=player))
-                        up_instance.games_played += 1
-                        up_instance.save()
-
-                        # pull player elo and confidence amounts
-                        player_elo = up_instance.mmr
-                        player_confidence = up_instance.confidence
-
-                        rating = Rating(float(player_elo), float(player_confidence))
-                        rating_group.append([rating])
-                        if player in winners:  # 0 is a winning player
-                            ranks.append(0)
-                        else:
-                            ranks.append(1)
-
-                    new_ratings = rate(rating_group, ranks)  # generate new elos based on trueskill
-
-                    for player in match_players:  # update every player with their new elos
-                        up_instance = UserPerformance.objects.get(user__pk=player)
-                        player_rating = new_ratings.pop(0)[0]  # needs two layers to index -> team -> player
-                        up_instance.mmr = player_rating.mu
-                        up_instance.confidence = player_rating.sigma
-                        up_instance.save()
-
-                    # UserPerformance.objects.filter(user__pk__in=match_players).update(games_played=F('games_played')+1)
-                    # UserPerformance.objects.filter(user__pk__in=winners).update(mmr=F('mmr') + 100)
-                    # UserPerformance.objects.filter(user__pk__in=losers).update(mmr=F('mmr') - 100)
-
-                    UserCode.objects.filter(user__pk__in=match_players).update(is_in_game=False)
-                    # todo: implement actual MMR calculation
-                    return Response(status=status.HTTP_201_CREATED)
+            if not isinstance(request.data.get("match_history", None), list):
                 return Response({"ok": False, "message": "Missing match history"},
                                 status=status.HTTP_400_BAD_REQUEST)
-            return Response({"ok": False, "message": "One or more winner not part of match"},
-                            status=status.HTTP_400_BAD_REQUEST)
-        return Response({"ok": False, "message": "No winners provided"},
-                        status=status.HTTP_400_BAD_REQUEST)
+
+            match_result = MatchResult()
+            match_result.time_started = match.allocated
+            match_result.players = Match.objects.get(pk=pk).players
+            match_result.winners = request.data["winners"]
+            match_result.match_events = request.data["match_history"]
+            match_result.save()
+
+            match.delete()
+
+            rating_group = []  # list of player ratings and their win/loss pos
+            ranks = []
+
+            for player in match_players:
+                player_code_instance = UserCode.objects.get(pk=player)
+                up_instance, _ = UserPerformance.objects.get_or_create(code=player_code_instance,
+                                                                       user=player_code_instance.user)
+                up_instance.games_played += 1
+                up_instance.save()
+
+                # pull player elo and confidence amounts
+                player_elo = up_instance.mmr
+                player_confidence = up_instance.confidence
+
+                rating = Rating(float(player_elo), float(player_confidence))
+                rating_group.append([rating])
+                if player in winners:  # 0 is a winning player
+                    ranks.append(0)
+                else:
+                    ranks.append(1)
+
+            new_ratings = rate(rating_group, ranks)  # generate new elos based on trueskill
+
+            for player in match_players:  # update every player with their new elos
+                up_instance = UserPerformance.objects.get(code__pk=player)
+                player_rating = new_ratings.pop(0)[0]  # needs two layers to index -> team -> player
+                up_instance.mmr = player_rating.mu
+                up_instance.confidence = player_rating.sigma
+                up_instance.save()
+
+            # UserPerformance.objects.filter(user__pk__in=match_players).update(games_played=F('games_played')+1)
+            # UserPerformance.objects.filter(user__pk__in=winners).update(mmr=F('mmr') + 100)
+            # UserPerformance.objects.filter(user__pk__in=losers).update(mmr=F('mmr') - 100)
+
+            UserCode.objects.filter(pk__in=match_players).update(is_in_game=False)
+            return Response(status=status.HTTP_201_CREATED)
+        if request.data["outcome"] == "fail":
+            if not isinstance(request.data.get("causes", None), list):
+                return Response({"ok": False, "message": "Missing failure causes"})
+            for player_code, cause in request.data["causes"].items():
+                print(player_code, cause)
+                if cause in ["timeout", "died"]:
+                    pass  # do something if a player times out
 
 
 # noinspection PyMethodMayBeStatic
@@ -154,10 +169,20 @@ class UserCodeViewSet(viewsets.ModelViewSet):
     serializer_class = UserCodeSerializer
     permission_classes = [permissions.IsAuthenticated]
 
+    @action(detail=True, permission_classes=[])
+    def download(self, request, pk=None):
+        user_code = UserCode.objects.filter(pk=pk).first()
+        if user_code is not None:
+            resp = HttpResponse(user_code.source_code.file, content_type="application/octet-stream")
+            resp['Content-Disposition'] = f'attachment; filename={os.path.basename(user_code.source_code.name)}'
+            return resp
+        return Response(status=status.HTTP_404_NOT_FOUND)  # this should not happen to the game runner (matchmaking)
+
 
 class UserPerformanceViewSet(viewsets.ModelViewSet):
     queryset = UserPerformance.objects.all()
     serializer_class = UserPerformanceSerializer
+
     # permission_classes = []
 
     def list(self, request, **kwargs):
